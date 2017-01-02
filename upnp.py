3e8ac4708354b78dee170ec5ca78a73ecf530dee
Monschichi <mail@pacific-state.de>
2017-01-02 21:18:22 +0100

    beautify

--- upnp.py
+++ upnp.py
@@ -7,7 +7,6 @@ from flask.ext.cache import Cache
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 app = Flask(__name__)
 cache.init_app(app)
-
 fc = fritzconnection.FritzConnection(address='10.0.0.1')
 
 
