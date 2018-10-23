0a6ba9bada7eb699976a8507f08a2ddf5f647c02
Sven Scharf <mail@pacific-state.de>
2018-10-23 18:28:41 +0200

    some more code style fixes and refactoring in JavaScript

--- static/fritz.js
+++ static/fritz.js
@@ -1,3 +1,5 @@
+/*eslint max-len: ["error", { "code": 140 }]*/
+
 function prepad(number, length) {
     return (" ".repeat(Math.max(0, length - number.toString().length)) + number);
 }
@@ -11,7 +13,8 @@ function uhr() {
 
 function fail(jqxhr, textStatus, error) {
     console.log("Status failed: " + textStatus + " " + error);
-    $("span#physical").addClass("fgred").removeClass("bgred bgyellow bggreen").text("error getting status");
+    $("span#physical").addClass("fgred").removeClass("bgred bgyellow bggreen")
+        .text("error getting status");
     $("span#logical").text("");
     $("span#up").text("");
     $("span#down").text("");
@@ -19,48 +22,14 @@ function fail(jqxhr, textStatus, error) {
     $("span#down-bar").css("width", "0%");
 }
 
-function update(json) {
-    let contype = json["physical"]["type"];
-    let indentlength = Math.max(4, contype.length);
+function up_down_text(updown, indentlength, json, length) {
+    let indent = indentlength - updown.length;
+    $("span#" + updown).text(
+        updown.toUpperCase() + " ".repeat(indent) + ":" + prepad((json["rate"][updown] / 1024).toFixed(1), length) + "/" +
+        Math.round(json["physical"]["rate"][updown] / 1024) + " kb/s");
+}
 
-    // Physical
-    let physical = $("span#physical");
-    physical.removeClass("fgred").text(contype + " ".repeat(indentlength - contype.length) + ":");
-    let physicalstatus = $("<span></span>");
-    if (json["physical"]["connected"]) {
-        physicalstatus.addClass("fggreen").text("✔");
-    } else {
-        physicalstatus.addClass("fgred").text("✘");
-    }
-    physical.append(physicalstatus);
-    let bw = $("<span></span>");
-    bw.addClass("small").text(Math.round(json["physical"]["rate"]["up"] * 8 / 1000000) + "/" + Math.round(json["physical"]["rate"]["down"] * 8 / 1000000) + " MBit/s");
-    physical.append(bw);
-
-    // Logical
-    let logical = $("span#logical");
-    logical.text("CON" + " ".repeat(indentlength - 3) + ":");
-    let logicalstatus = $("<span></span>");
-    if (json["logical"]["connected"]) {
-        logicalstatus.addClass("fggreen").text("✔");
-    } else {
-        logicalstatus.addClass("fgred").text("✘" + json["logical"]["lasterror"]);
-    }
-    logical.append(logicalstatus);
-    let ips = $("<span></span>");
-    ips.addClass("small").text(json["logical"]["ipv4"] + " " + json["logical"]["ipv6"]["prefix"] + "/" + json["logical"]["ipv6"]["length"]);
-    logical.append(ips);
-
-    // get rate lenght
-    let maxrate = Math.max(Math.ceil(json["physical"]["rate"]["up"] / 1024), Math.ceil(json["physical"]["rate"]["down"] / 1024));
-    let length = maxrate.toString().length + 3;
-
-    // UP
-    // Text
-    $("span#up").text("UP" + " ".repeat(indentlength - 2) + ":" + prepad((json["rate"]["up"] / 1024).toFixed(1), length) + "/" + Math.round(json["physical"]["rate"]["up"] / 1024) + " kb/s");
-    // Rate
-    let uprate = json["rate"]["up"] / json["physical"]["rate"]["up"] * 100;
-    let upbar = $("span#up-bar");
+function up_down_rate(upbar, uprate) {
     upbar.css("width", uprate + "%");
     if (uprate >= 90) {
         upbar.addClass("bgred").removeClass("bgyellow bggreen");
@@ -69,21 +38,60 @@ function update(json) {
     } else {
         upbar.addClass("bggreen").removeClass("bgyellow bgred");
     }
+}
+
+function status_icon(json, type) {
+    let status = $("<span></span>");
+    if (json[type]["connected"]) {
+        status.addClass("fggreen").text("✔");
+    } else {
+        status.addClass("fgred").text("✘");
+    }
+    return status;
+}
+
+function status_text(type, contype, indentlength, json) {
+    let span = $("span#" + type);
+    let indent = indentlength - contype.length;
+    span.removeClass("fgred").text(contype + " ".repeat(indent) + ":");
+    span.append(status_icon(json, type));
+    let bw = $("<span></span>");
+    if (type === "physical") {
+        bw.addClass("small").text(
+            Math.round(json["physical"]["rate"]["up"] * 8 / 1000000) + "/" + Math.round(json["physical"]["rate"]["down"] * 8 / 1000000) +
+            " MBit/s");
+    } else {
+        bw.addClass("small")
+            .text(json["logical"]["ipv4"] + " " + json["logical"]["ipv6"]["prefix"] + "/" + json["logical"]["ipv6"]["length"]);
+    }
+    span.append(bw);
+}
+
+function update(json) {
+    let contype = json["physical"]["type"];
+    let indentlength = Math.max(4, contype.length);
+
+    // Physical
+    status_text("physical", contype, indentlength, json);
+
+    // Logical
+    status_text("logical", "CON", indentlength, json);
+
+    // get rate lenght
+    let maxrate = Math.max(Math.ceil(json["physical"]["rate"]["up"] / 1024), Math.ceil(json["physical"]["rate"]["down"] / 1024));
+    let length = maxrate.toString().length + 3;
+
+    // UP
+    // Text
+    up_down_text("up", indentlength, json, length);
+    // Rate
+    up_down_rate($("span#up-bar"), json["rate"]["up"] / json["physical"]["rate"]["up"] * 100);
 
     // Down
     // Text
-    $("span#down").text("DOWN" + " ".repeat(indentlength - 4) + ":" + prepad((json["rate"]["down"] / 1024).toFixed(1), length) + "/" + Math.round(json["physical"]["rate"]["down"] / 1024) + " kb/s");
+    up_down_text("down", indentlength, json, length);
     // Rate
-    let downrate = json["rate"]["down"] / json["physical"]["rate"]["down"] * 100;
-    let downbar = $("span#down-bar");
-    downbar.css("width", downrate + "%");
-    if (downrate >= 90) {
-        downbar.addClass("bgred").removeClass("bgyellow bggreen");
-    } else if (downrate >= 70) {
-        downbar.addClass("bgyellow").removeClass("bggreen bgred");
-    } else {
-        downbar.addClass("bggreen").removeClass("bgyellow bgred");
-    }
+    up_down_rate($("span#down-bar"), json["rate"]["down"] / json["physical"]["rate"]["down"] * 100);
 }
 
 function myTimer() {
